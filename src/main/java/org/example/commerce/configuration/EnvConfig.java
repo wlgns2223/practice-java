c3fdf64aae3c536ba117e9fd8afc03bad0ba306b
심지훈 <wlgns2223@gmail.com>
2025-11-03 22:13:02 +0900

    chore: initial commit

---
+++ src/main/java/org/example/commerce/configuration/EnvConfig.java
@@ -0,0 +1,12 @@
+package org.example.commerce.configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+    static {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),entry.getValue()));
+    }
+}
--- src/main/java/org/example/commerce/controller/UserController.java
+++ src/main/java/org/example/commerce/controller/UserController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.commerce.dto.UserRequestDto;
 import org.example.commerce.dto.UserResponseDto;
 import org.example.commerce.service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
