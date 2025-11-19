3a9bf1fdfff412deb5c9b735325d336f86c50783
심지훈 <wlgns2223@gmail.com>
2025-11-19 23:06:50 +0900

    feat: cart

--- src/main/java/org/example/commerce/entity/Cart.java
+++ src/main/java/org/example/commerce/entity/Cart.java
@@ -1,9 +1,25 @@
 package org.example.commerce.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 public class Cart extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "cart_item",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
 }
